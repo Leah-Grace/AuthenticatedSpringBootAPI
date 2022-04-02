dd1596d67d61e27d4e594a8a509f5dd94ca1e11d
Leah Grace <Leahgdg@hotmail.com>
2022-04-02 16:41:38 -0400

    "First Commit"

---
+++ src/main/java/com/LeahGrace/AuthenticatedSpringBootAPI/AuthenticatedSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.LeahGrace.AuthenticatedSpringBootAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatedSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatedSpringBootApiApplication.class, args);
+	}
+
+}
